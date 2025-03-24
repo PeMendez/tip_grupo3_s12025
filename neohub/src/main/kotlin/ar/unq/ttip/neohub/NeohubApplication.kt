eeefc98e034eae8619a1fc000617483be1a1928a
Martin Enriquez <martin.hernan.enriquez@gmail.com>
2025-03-24 15:38:02 -0300

    commit inicial para el proyecto spring boot

---
+++ neohub/src/main/kotlin/ar/unq/ttip/neohub/NeohubApplication.kt
@@ -0,0 +1,11 @@
+package ar.unq.ttip.neohub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NeohubApplication
+
+fun main(args: Array<String>) {
+	runApplication<NeohubApplication>(*args)
+}
---
+++ neohub/src/test/kotlin/ar/unq/ttip/neohub/NeohubApplicationTests.kt
@@ -0,0 +1,13 @@
+package ar.unq.ttip.neohub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NeohubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
