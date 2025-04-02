39122b9149b417d11182c3fe31736ef4c7ddc715
Martin Enriquez <martin.hernan.enriquez@gmail.com>
2025-04-02 13:02:38 -0300

    Se agregan los permisos de CORS para que el front pueda hacer solicitudes

---
+++ neohub/src/main/kotlin/ar/unq/ttip/neohub/security/CorsConfig.kt
@@ -0,0 +1,22 @@
+package ar.unq.ttip.neohub.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CorsConfig {
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowedOrigins = listOf("http://localhost:5173") //Donde levanta la pagina
+        config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        config.allowedHeaders = listOf("*")
+        config.allowCredentials = true
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+}
