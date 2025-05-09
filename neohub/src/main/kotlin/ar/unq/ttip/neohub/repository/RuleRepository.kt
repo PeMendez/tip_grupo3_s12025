db07dccebb5adee0973139aea36dd1eaa4a9e847
Martin Enriquez <martin.hernan.enriquez@gmail.com>
2025-05-09 18:00:19 -0300

    Agregamos query para que filtre por device en la base de datos, en lugar de traer todas y filtrar en memoria.

--- neohub/src/main/kotlin/ar/unq/ttip/neohub/repository/RuleRepository.kt
+++ neohub/src/main/kotlin/ar/unq/ttip/neohub/repository/RuleRepository.kt
@@ -2,7 +2,17 @@ package ar.unq.ttip.neohub.repository
 
 import ar.unq.ttip.neohub.model.ruleEngine.Rule
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
 
+@Repository
 interface RuleRepository : JpaRepository<Rule, Long> {
-
+    @Query("""
+        SELECT DISTINCT r
+        FROM Rule r
+        LEFT JOIN r.conditions c
+        LEFT JOIN r.actions a
+        WHERE c.device.id = :deviceId OR a.device.id = :deviceId
+    """)
+    fun findRulesByDeviceId(deviceId: Long): List<Rule>
 }
