3b0ca9b8e7e212c8e2535c97097bb8d9c5e1832c
Martin Enriquez <martin.hernan.enriquez@gmail.com>
2025-06-28 12:26:02 -0300

    FIXED: Centralizada la url en el archivo de entorno para poder cambiarla.

--- frontend/src/api/deviceService.js
+++ frontend/src/api/deviceService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/devices';
+const API_URL = import.meta.env.VITE_API_URL + '/devices';
 
 export const getUnconfiguredDevices = async (token) => {
     try {
--- frontend/src/api/roomService.js
+++ frontend/src/api/roomService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/room';
+const API_URL = import.meta.env.VITE_API_URL + '/room';
 
 export const getRoomDetails = async (roomId, token) => {
     try {
